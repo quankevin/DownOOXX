315fb945c6b51d05872ff295fc1ae6950d2f124b
quankevin <quankevin@163.com>
2016-06-20 09:59:31 +0800

    Initial Commit

---
+++ DownXXOO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DownXXOO
+//
+//  Created by 维农 on 16/6/20.
+//  Copyright © 2016年 维农-quan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
